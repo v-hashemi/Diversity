142e89829fff42eb8de4d030db9305ca124cb629
v-hashemi <v-hashemi@users.noreply.github.com>
2014-12-31 14:43:40 +0100

    Create Def.h

---
+++ Def.h
@@ -0,0 +1,35 @@
+#ifndef _DEF_
+#define _DEF_
+
+#pragma warning(disable: 4786)
+
+#include <cmath>
+#include <iomanip>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <vector>
+#include <map>
+#include <set>
+
+#include <valarray>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+using namespace std;
+
+#include <limits>
+const double INF = numeric_limits<double>::infinity();
+const int Inf = numeric_limits<int>::max();
+const double EPS = 0.00001;
+
+typedef int idType;
+typedef set <idType> idSet;
+typedef vector <idType> List; //Path;
+typedef set <List> setList;
+
+#endif
